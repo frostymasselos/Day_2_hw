2aeb5888e0b1ad4ab19d29eee399253030ed3389
Ryan <ryanfrost2015@gmail.com>
2019-10-15 16:41:34 +0100

    Seventh function

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -21,3 +21,7 @@ end
 def length_of_string(anything)
   "A string of length 21".length
 end
+
+def join_string(string_1, string_2)
+  "Mary had a little lamb, " + "its fleece was white as snow"
+end
